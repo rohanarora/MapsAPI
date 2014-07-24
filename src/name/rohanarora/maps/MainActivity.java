e3f8fe238aaa738348220d62fdfda0cc456d0f8e
Rohan R. Arora <rohan@rohanarora.name>
2014-07-24 18:46:02 -0500

    Google Maps loaded in the app

--- src/name/rohanarora/maps/MainActivity.java
+++ src/name/rohanarora/maps/MainActivity.java
@@ -10,6 +10,8 @@ public class MainActivity extends Activity
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        // setContentView(R.layout.main);
+	setContentView(R.layout.activity_main);
+
     }
 }
